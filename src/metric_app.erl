7a7ed208fe9d808c9ce405172fad4504fc03200a
Anton Zaec <antonrumato@yandex.ru>
2017-02-13 18:30:55 +0300

    Validation of metric value

--- src/metric_app.erl
+++ src/metric_app.erl
@@ -11,13 +11,13 @@ start(_StartType, _StartArgs) ->
 stop(_State) ->
     ok.
 
-report(MetricName, Value) when is_binary(MetricName) -> 
+report(MetricName, Value) when is_binary(MetricName) and is_number(Value) -> 
 	{ok, Interval} = application:get_env(metric, interval_ms),
 	MetricServer = metric_manager:get_or_create_mserver(MetricName, Interval),
 	metric_server:report(MetricServer, MetricName, Value),
 	ok.
 
-average(MetricName) when is_binary(MetricName) ->
+average(MetricName) when is_binary(MetricName)->
 	{ok, Interval} = application:get_env(metric, interval_ms),
 	MetricServer = metric_manager:get_or_create_mserver(MetricName, Interval),
 	metric_server:average(MetricServer, MetricName).
