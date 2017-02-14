67d51da0324da23f34221e747d2f7f095476c0cb
Anton Zaec <antonrumato@yandex.ru>
2017-02-14 11:13:21 +0300

    Replace state type for metric's server. Now it is tuple, not map.

--- src/metric_server.erl
+++ src/metric_server.erl
@@ -30,45 +30,57 @@ average(MetricServer, MetricName) ->
 	end.
 
 init([MetricName, Interval]) ->
-	{ok, #{
-		name => MetricName, 
-		interval => Interval, 
-		values => [], 
-		sum => 0.0, 
-		count => 0}}.
+	{ok, {
+		MetricName, %name
+		Interval, %interval
+		[], %values
+		0.0, %sum 
+		0}}. %count.
 
-handle_call({metric_average, MetricName}, _From, #{name := MetricName} = State) ->
+handle_call({metric_average, MetricName}, _From, 
+		{MetricName, _, _, _, _} = State) ->
 	?PRINT_DEBUG("State when average: ~p. Pid ~p.~n", [State, self()]),
 	NewState = update_state_for_interval(State),
-	#{sum := Sum, count := Count} = NewState,
+	{_, _, _, Sum, Count} = NewState,
 	case Count of
 		0 -> {reply, no_data, NewState};
 		_ -> {reply, Sum/Count, NewState}
 	end.
 
-handle_cast({metric_report, MetricName, Value}, #{name := MetricName} = State) ->
+handle_cast({metric_report, MetricName, Value}, 
+		{MetricName, _, _, _, _} = State) ->
 	?PRINT_DEBUG("State when report: ~p. Pid ~p.~n", [State, self()]),
-	NewState = update_state_for_interval(State),
-	#{values := Values, sum := Sum, count := Count} = NewState,
+	UpdatedState = update_state_for_interval(State),
+	{_, _, Values, Sum, Count} = UpdatedState,
 	CurTimeMs = erlang:system_time(milli_seconds),
-	{noreply, NewState#{
-		values := [{CurTimeMs, Value}|Values], 
-		sum := Sum + Value, 
-		count := Count + 1}}.
+	NewState = update_state(UpdatedState, Count + 1, 
+		Sum + Value, [{CurTimeMs, Value}|Values]),
+	{noreply, NewState}.
 
-update_state_for_interval(#{interval := Interval, values := Values} = State) ->
+update_state_for_interval({_, Interval, Values, _, _} = State) ->
 	CurTimeMs = erlang:system_time(milli_seconds),
 	MinTime = CurTimeMs - Interval,
 	NewState = remove_values_before(
-		MinTime, State#{values := lists:reverse(Values)}),
-	#{values := NewValues} = NewState,
-	NewState#{values := lists:reverse(NewValues)}.
+		MinTime, update_state_value(State, lists:reverse(Values))),
+	update_state_value(NewState, lists:reverse(element(3, NewState))).
 
-remove_values_before(MinTime, #{
-		values := [{ValTime, Val}|Values], 
-		sum := Sum, 
-		count := Count} = State) when ValTime =< MinTime ->
-	remove_values_before(MinTime, State#{
-		values := Values, sum := Sum - Val, count := Count - 1});
-remove_values_before(MinTime, State) ->
+remove_values_before(MinTime, 
+		{_, _, [{ValTime, Val}|Values], Sum, Count} = State) when ValTime =< MinTime ->
+	NewState = update_state(State, Count - 1, Sum - Val, Values),
+	remove_values_before(MinTime, NewState);
+remove_values_before(_MinTime, State) ->
 	State.
+
+update_state(State, Count, Sum, Values) ->
+	S1 = update_state_count(State, Count),
+	S2 = update_state_sum(S1, Sum),
+	update_state_value(S2, Values).
+
+update_state_value(State, NewValue) ->
+	setelement(3, State, NewValue).
+
+update_state_count(State, NewCount) ->
+	setelement(5, State, NewCount).
+
+update_state_sum(State, NewSum) ->
+	setelement(4, State, NewSum).
